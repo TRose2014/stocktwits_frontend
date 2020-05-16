c3578744ac85783d150c037a5f3b519dd410bcfd
Tia Rose <shortyshort789@aol.com>
2020-05-15 17:29:16 -0700

    working on CORS

--- src/App.js
+++ src/App.js
@@ -8,29 +8,29 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      body: {'test': 'test'}, // this is the body from /user
+      body: {}, // this is the body from /user
       heroku_url: 'https://stockwits-backend.herokuapp.com'
     };
   }
 
-  componentDidMount() {
-    fetch(`${this.state.heroku_url}/user`, {
-      credentials: 'include', // fetch won't send cookies unless you set credentials
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-      .then((response) => console.log('response', response))
-      .then((response) => response.json())
-      .then((response) => this.setState(
-        {
-          body: response,
-        },
-      ))
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  // componentDidMount() {
+  //   fetch(`${this.state.heroku_url}/user`, {
+  //     credentials: 'include', // fetch won't send cookies unless you set credentials
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   })
+  //     .then((response) => console.log('response', response))
+  //     .then((response) => response.json())
+  //     .then((response) => this.setState(
+  //       {
+  //         body: response,
+  //       },
+  //     ))
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
 
   render() {
     console.log('response', this.state.body)
