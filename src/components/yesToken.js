b629471b78d856e29e1c6eb66ee85784a2b3bf84
Tia Rose <shortyshort789@aol.com>
2020-05-17 22:36:18 -0700

    set up add/delete buttons

--- src/components/yesToken.js
+++ src/components/yesToken.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../index.css';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardContent, Button } from '@material-ui/core';
 
 export default class YesToken extends React.Component {
+  constructor(props) {
+    super(props);
+      this.addStock = this.addStock.bind(this);
+      this.removeStock = this.removeStock.bind(this);
+  }
+
+  addStock() {
+    console.log('Added stock');
+  }
+
+  removeStock() {
+    console.log('Removed stock');
+  }
   render() {
     return (
+      <>
       <Card>
         <CardContent>
         <h1>{this.props.title}</h1>
@@ -12,6 +26,9 @@ export default class YesToken extends React.Component {
         <h1>{this.props.exchange}</h1>
         </CardContent>
       </Card>
+      <Button onClick={this.addStock}>Add Stock</Button>
+      <Button onClick={this.removeStock}>Remove Stock</Button>
+      </>
     )
   }
 }
